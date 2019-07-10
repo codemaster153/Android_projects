3383fcc64cb13ac5be3e5f0b5d179bb22e37daca
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-07-10 12:05:07 +0530

    migrated to android x

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -1,30 +1,12 @@
 package com.lkb.baseandroidproject
 
-import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.*
-
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    private val mJob = Job()
-    private val uiScope = CoroutineScope(Dispatchers.Main + mJob)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        uiScope.launch {
-            showDelayedToast()
-        }
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mJob.cancel()
-    }
-
-    private suspend fun showDelayedToast() {
-        delay(1_000)
-        Toast.makeText(this@MainActivity, "From coroutine", Toast.LENGTH_SHORT).show()
     }
 }
