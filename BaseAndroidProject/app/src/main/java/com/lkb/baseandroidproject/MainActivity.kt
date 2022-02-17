3646fc5049813f70bc6ae5258f6434981d0a438e
lalit <lalit143.er@gmail.com>
2022-02-17 14:13:41 +0530

    view binding added

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -2,12 +2,15 @@ package com.lkb.baseandroidproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.lkb.baseandroidproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
     }
 }
