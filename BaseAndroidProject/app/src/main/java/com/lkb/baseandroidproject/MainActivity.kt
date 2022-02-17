ab1badbbf87eea704ce731d0ccdbe508ee0efc62
lalit <lalit143.er@gmail.com>
2022-02-17 14:01:40 +0530

    gradle fixed

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.lkb.baseandroidproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
