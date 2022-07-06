7f3b3714ad2b571b4ee06c05a0ff54b7999ccd12
lalit <lalit143.er@gmail.com>
2022-07-06 13:44:52 +0530

    updated code to display a text in a box

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -3,10 +3,14 @@ package com.lkb.baseandroidproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.lkb.baseandroidproject.ui.theme.BaseAndroidProjectTheme
 
@@ -17,7 +21,9 @@ class MainActivity : ComponentActivity() {
             BaseAndroidProjectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Text(text = "Hello world", modifier = Modifier.align(Alignment.Center))
+                    }
                 }
             }
         }
