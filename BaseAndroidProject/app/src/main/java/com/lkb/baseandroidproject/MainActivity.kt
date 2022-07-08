6cc4fdbfbb6652666c38905dac194dc742ce201a
lalit <lalit143.er@gmail.com>
2022-07-08 21:02:23 +0530

    timer created using flow builder

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -9,9 +9,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.lkb.baseandroidproject.ui.theme.BaseAndroidProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +21,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BaseAndroidProjectTheme {
-                // A surface container using the 'background' color from the theme
+                val viewModel = viewModel<MainViewModel>()
+                val countDownTimer = viewModel.countDownFlow.collectAsState(
+                    initial = 10
+                )
                 Surface(color = MaterialTheme.colors.background) {
                     Box(modifier = Modifier.fillMaxSize()) {
-                        Text(text = "Hello world!!", modifier = Modifier.align(Alignment.Center))
+                        Text(text = countDownTimer.value.toString(), modifier = Modifier.align(Alignment.Center))
                     }
                 }
             }
