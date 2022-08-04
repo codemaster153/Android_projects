725526bed56aa7530c06081e0b28efeb1714b341
lalit <lalit143.er@gmail.com>
2022-08-04 21:59:22 +0530

    hilt dependency added

---
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/App.kt
@@ -0,0 +1,8 @@
+package com.lkb.baseandroidproject
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+}
--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -2,12 +2,15 @@ package com.lkb.baseandroidproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.lkb.baseandroidproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     lateinit var ss: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
        this::ss.isInitialized
     }
 }
