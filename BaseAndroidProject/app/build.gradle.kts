9f37c7ad45a8c424f63ade31289d60f546644e77
lalitkumarb <lalit143.er@gmail.com>
2022-12-06 20:36:07 +0530

    gradle dependency added

--- BaseAndroidProject/app/build.gradle.kts
+++ BaseAndroidProject/app/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 }
 
 android {
-    compileSdk = 32
+    compileSdk = 33
     buildToolsVersion = "30.0.3"
 
     defaultConfig {
         applicationId = "com.lkb.baseandroidproject"
         minSdk = 21
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -47,31 +47,26 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.4.0")
-//    implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
-//    implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
-//    implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
-//    implementation("androidx.compose.foundation:foundation:${rootProject.extra["compose_version"]}")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.7.0")
     // Integration with activities
-    implementation ("androidx.activity:activity-compose:1.5.1")
+    implementation ("androidx.activity:activity-compose:1.6.1")
     // Compose Material Design
-    implementation ("androidx.compose.material:material:1.2.1")
+    implementation ("androidx.compose.material:material:1.3.1")
     // Animations
-    implementation ("androidx.compose.animation:animation:1.2.1")
+    implementation ("androidx.compose.animation:animation:1.3.1")
     // Tooling support (Previews, etc.)
-    implementation ("androidx.compose.ui:ui-tooling:1.2.1")
+    implementation ("androidx.compose.ui:ui-tooling:1.3.1")
     // Integration with ViewModels
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
 
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.0-rc01")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.activity:activity-compose:1.7.0-alpha02")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
     testImplementation("junit:junit:4.+")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.4")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
     // UI Tests
-    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.2.1")
-    //androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.3.1")
 }
