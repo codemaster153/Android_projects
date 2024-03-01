832232d4da8fe31880070fabc898952f773f42b9
bluesky15 <lalit143.er@gmail.com>
2024-03-01 13:54:19 +0530

    pull to refresh done

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -3,49 +3,57 @@ package com.lkb.baseandroidproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ListItem
 import androidx.compose.material.Text
-import androidx.compose.runtime.produceState
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.lkb.baseandroidproject.ui.theme.BaseAndroidProjectTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private val service = PostsService.create()
 
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val posts = produceState<List<PostResponse>>(
-                initialValue = emptyList(),
-                producer = {
-                    value = service.getPosts()
-                }
-            )
             BaseAndroidProjectTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    LazyColumn {
-                        items(posts.value) {
-                            Column(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(16.dp)
-                            ) {
-                                Text(text = it.title, fontSize = 20.sp)
-                                Spacer(modifier = Modifier.height(4.dp))
-                                Text(text = it.body, fontSize = 14.sp)
+                val refreshScope = rememberCoroutineScope()
+                var refreshing by remember { mutableStateOf(false) }
+                var itemCount by remember { mutableIntStateOf(15) }
+
+                fun refresh() = refreshScope.launch {
+                    refreshing = true
+                    delay(1500)
+                    itemCount += 5
+                    refreshing = false
+                }
+
+                val state = rememberPullRefreshState(refreshing, ::refresh)
+
+                Box(Modifier.pullRefresh(state)) {
+                    LazyColumn(Modifier.fillMaxSize()) {
+                        if (!refreshing) {
+                            items(itemCount) {
+                                ListItem { Text(text = "Item ${itemCount - it}") }
                             }
                         }
                     }
+                    PullRefreshIndicator(refreshing, state, Modifier.align(Alignment.TopCenter))
                 }
             }
         }
